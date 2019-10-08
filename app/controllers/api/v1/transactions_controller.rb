4a10aa1a8f0585ba32326d27e71c0a8d557f5113
hmalik88 <hbmalik88@gmail.com>
2019-10-08 16:00:14 -0400

    tweaked transactions controller

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,17 +1,17 @@
 class Api::V1::TransactionsController < ApplicationController
 
 def create
-  @transaction = Transaction.new(transaction_params)
+  @transaction = Transaction.new(transactions_params)
   if @transaction.valid?
     @transaction.save
-    render json: {transaction: @transaction}, status: :created
+    @user = User.find_by(id: transactions_params[:user_id])
+    @deduction = transactions_params[:quantity] * transactions_params[:price]
+    @user.balance -= @deduction
+    @user.save
+    render json: {transaction: @transaction, balance: @user.balance}, status: :created
   else
     render json: {error: 'transaction failed'}, status: :not_acceptable
   end
-  @user = User.find_by(id: transaction_params[:user_id])
-  @deduction = transaction_params[:quantity] * transaction_params[:price]
-  @user.balance -= @deduction
-  @user.save
 end
 
 
